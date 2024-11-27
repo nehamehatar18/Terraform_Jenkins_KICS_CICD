9bc56c4b05348b28f58cd2413e57f39c990f4891
NeeharikaRN <n.neeharikareddy@gmail.com>
2024-11-27 14:38:07 +0530

    Delete scripts directory

--- scripts/kics_scan.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-kics scan --path *.tf -q /root/kics/assets/queries --report-formats "html,json" -o ./output  --output-name kics-result-tf-cicd
--- scripts/kics_version.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export PATH=$PATH:/root/kics/bin
-kics version
